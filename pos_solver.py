40c572697131f23d8ad17d29c9c85dc966ef305d
Sarvothaman Madhavan <madhavas@iu.edu>
2016-10-31 22:47:13 -0400

    Calculated emission probabilities. Probability Testing is pending

--- pos_solver.py
+++ pos_solver.py
@@ -24,7 +24,7 @@ class Solver:
     prior = {}
     pos_init_probabilities = {}
     pos_transition_probabilities = {}
-
+    emission_probabilities={}
     # Calculate the log of the posterior probability of a given sentence
     #  with a given part-of-speech labeling
     def posterior(self, sentence, label):
@@ -36,19 +36,20 @@ class Solver:
         prior_counts = {}
         pos_init_counts = {}
         pos_transition_counts = {}
+        emission_counts={}
         word_count = 0
         sentence_count = 0
+
         for word_list, pos_list in data:
             counter = collections.Counter(pos_list)
             first_state_flag = 1
-            for pos, count in counter.items():
+            for pos , word in zip(pos_list, word_list):
                 if first_state_flag == 1:
                     sentence_count += 1
                     if pos in pos_init_counts:
                         pos_init_counts[pos] += 1
                     else:
                         pos_init_counts[pos] = 1
-
                     first_state_flag = 0
                 else:
                     if previous_pos in pos_transition_counts:
@@ -59,27 +60,48 @@ class Solver:
                     else:
                         pos_transition_counts[previous_pos] = {}
                         pos_transition_counts[previous_pos][pos] = 1
+                previous_pos = pos
+            if pos in emission_counts:
+                if word in emission_counts[pos]:
+                    emission_counts[pos][word] += 1
+                else:
+                    emission_counts[pos][word] = 1
+            else:
+                emission_counts[pos] = {}
+                emission_counts[pos][word] = 1
 
+            for pos, count in counter.items():
                 word_count += count
                 if pos in prior_counts:
                     prior_counts[pos] += count
                 else:
                     prior_counts[pos] = count
-                previous_pos = pos
+
         for pos, count in prior_counts.items():
             self.prior[pos] = (1.0 * count) / word_count
+
         for pos, count in pos_init_counts.items():
             self.pos_init_probabilities[pos] = (1.0 * count) / sentence_count
+
         for previous_pos, pos_dict in pos_transition_counts.items():
             current_count = sum(pos_dict.values())
             self.pos_transition_probabilities[previous_pos] = {}
             for pos, count in pos_dict.items():
                 self.pos_transition_probabilities[previous_pos][pos] = (1.0 * count) / current_count
+
+        for pos, word_dict in emission_counts.items():
+            current_count = sum(word_dict.values())
+            self.emission_probabilities[pos] = {}
+            for word, count in word_dict.items():
+                self.emission_probabilities[pos][word] = (1.0 * count) / current_count
+
         pprint.pprint(self.prior)
         pprint.pprint("================")
         pprint.pprint(self.pos_init_probabilities)
         pprint.pprint("================")
         pprint.pprint(self.pos_transition_probabilities)
+        pprint.pprint("================")
+        pprint.pprint(self.emission_probabilities)
         pprint.pprint("================")
 
     # Functions for each algorithm.
