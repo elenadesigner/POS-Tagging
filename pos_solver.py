22c20ccfe45a971319a3623672899b0033da8b4d
Sarvothaman Madhavan <madhavas@iu.edu>
2016-11-04 16:08:06 -0400

    Added complex model state transition probability calculation, fixed formatting and added comments

--- pos_solver.py
+++ pos_solver.py
@@ -25,7 +25,9 @@ class Solver:
     prior = {}
     pos_init_probabilities = {}
     pos_transition_probabilities = {}
+    pos_complex_transition_probabilities = {}
     emission_probabilities = {}
+
     emission_cost = {}
     pos_transition_cost = {}
 
@@ -40,9 +42,12 @@ class Solver:
         prior_counts = {}
         pos_init_counts = {}
         pos_transition_counts = {}
+        pos_complex_transition_counts = {}
         emission_counts = {}
         word_count = 0
         sentence_count = 0
+        trigram_pos = None
+        previous_pos = None
 
         for word_list, pos_list in data:
             first_state_flag = 1
@@ -65,6 +70,19 @@ class Solver:
                     else:
                         pos_transition_counts[previous_pos] = {}
                         pos_transition_counts[previous_pos][pos] = 1
+
+                    # Calculate transition counts for complex model
+                    if trigram_pos is not None:
+                        if (trigram_pos, previous_pos) in pos_complex_transition_counts:
+                            if pos in pos_complex_transition_counts[(trigram_pos, previous_pos)]:
+                                pos_complex_transition_counts[(trigram_pos, previous_pos)][pos] += 1
+                            else:
+                                pos_complex_transition_counts[(trigram_pos, previous_pos)][pos] = 1
+                        else:
+                            pos_complex_transition_counts[(trigram_pos, previous_pos)] = {}
+                            pos_complex_transition_counts[(trigram_pos, previous_pos)][pos] = 1
+                    trigram_pos = previous_pos
+
                 # Calculate emission counts
                 if pos in emission_counts:
                     if word in emission_counts[pos]:
@@ -82,30 +100,45 @@ class Solver:
                     prior_counts[pos] = 1
                 previous_pos = pos
 
+        # Convert prior counts to probabilities
         for pos, count in prior_counts.items():
             self.prior[pos] = (1.0 * count) / word_count
 
+        # List of prior value
         prior_pos = set(self.prior.keys())
         to_add = prior_pos.difference(self.pos_init_probabilities.keys())
+
+        # Set missing pos to very small value (Laplace smoothing)
         for item in to_add:
             self.pos_init_probabilities[item] = 0.00001
 
+        # Convert initial state counts to probabilities
         for pos, count in pos_init_counts.items():
             self.pos_init_probabilities[pos] = (1.0 * count) / sentence_count
+
         prior_pos_copy = prior_pos.copy()
+        # Convert state transitions counts to probabilities
         for previous_pos, pos_dict in pos_transition_counts.items():
             prior_pos.remove(previous_pos)
             current_count = sum(pos_dict.values())
             self.pos_transition_probabilities[previous_pos] = {}
             self.pos_transition_cost[previous_pos] = {}
-            strike_off=prior_pos_copy.copy()
+            strike_off = prior_pos_copy.copy()
             for current_pos, count in pos_dict.items():
+                # Strike off pos values seen so far
                 strike_off.remove(current_pos)
+
                 self.pos_transition_probabilities[previous_pos][current_pos] = ((1.0 * count) / current_count) + 0.00001
-                self.pos_transition_cost[previous_pos][current_pos] = math.log(1.0/self.pos_transition_probabilities[previous_pos][current_pos])
+                self.pos_transition_cost[previous_pos][current_pos] = math.log(
+                    1.0 / self.pos_transition_probabilities[previous_pos][current_pos])
+
+            # Set very small values for transitions not seen so far
             for inner_item in strike_off:
                 self.pos_transition_probabilities[previous_pos][inner_item] = 0.00001
-                self.pos_transition_cost[previous_pos][inner_item] = math.log(1.0/self.pos_transition_probabilities[previous_pos][inner_item])
+                self.pos_transition_cost[previous_pos][inner_item] = math.log(
+                    1.0 / self.pos_transition_probabilities[previous_pos][inner_item])
+
+        # Set very small values for transitions not seen so far
         for item in prior_pos:
             self.pos_transition_probabilities[item] = {}
             self.pos_transition_cost[item] = {}
@@ -114,6 +147,7 @@ class Solver:
                 self.pos_transition_cost[item][inner_item] = math.log(
                     1.0 / self.pos_transition_probabilities[item][inner_item])
 
+        # Convert emission counts to probabilities
         for pos, word_dict in emission_counts.items():
             current_count = sum(word_dict.values())
             self.emission_probabilities[pos] = {}
@@ -121,6 +155,13 @@ class Solver:
             for word, count in word_dict.items():
                 self.emission_probabilities[pos][word] = ((1.0 * count) / current_count) + 0.00001
                 self.emission_cost[pos][word] = math.log(1.0 / self.emission_probabilities[pos][word])
+        # Convert state transitions counts to probabilities
+        for pos_combination, pos_dict in pos_complex_transition_counts.items():
+            current_count = sum(pos_dict.values())
+            self.pos_complex_transition_probabilities[pos_combination] = {}
+            for current_pos, count in pos_dict.items():
+                self.pos_complex_transition_probabilities[pos_combination][current_pos] = ((
+                                                                                           1.0 * count) / current_count) + 0.00001
 
     # Functions for each algorithm.
     #
@@ -149,43 +190,43 @@ class Solver:
         for idx, pos_type in enumerate(self.prior.keys()):
             word = sentence[0]
             if sentence[0] in self.emission_cost[pos_type]:
-                veterbi[0][pos_type] = ([],math.log(1/self.pos_init_probabilities[pos_type]) + self.emission_cost[pos_type][word])
+                veterbi[0][pos_type] = (
+                    [], math.log(1 / self.pos_init_probabilities[pos_type]) + self.emission_cost[pos_type][word])
             else:
-                veterbi[0][pos_type] = ([],math.log(1/self.pos_init_probabilities[pos_type]) + math.log(1/0.00001))
-        for index in range(1,len(sentence)):
+                veterbi[0][pos_type] = ([], math.log(1 / self.pos_init_probabilities[pos_type]) + math.log(1 / 0.00001))
+        for index in range(1, len(sentence)):
             word = sentence[index]
             veterbi[index] = {}
-            for idx,pos_type in enumerate(self.prior.keys()):
+            for idx, pos_type in enumerate(self.prior.keys()):
                 prob_values = []
-                for idx1,pos_type1 in enumerate(self.prior.keys()):
-                    prob_values.append(veterbi[index-1][pos_type1][1] + self.pos_transition_cost[pos_type1][pos_type])
+                for idx1, pos_type1 in enumerate(self.prior.keys()):
+                    prob_values.append(veterbi[index - 1][pos_type1][1] + self.pos_transition_cost[pos_type1][pos_type])
                 min_value = min(prob_values)
                 min_pos = self.prior.keys()[prob_values.index(min_value)]
-                path = veterbi[index-1][min_pos][0][:]
+                path = veterbi[index - 1][min_pos][0][:]
                 path.append(min_pos)
                 if word in self.emission_cost[pos_type]:
-                    veterbi[index][pos_type] = (path,min(prob_values) + self.emission_cost[pos_type][word])
+                    veterbi[index][pos_type] = (path, min(prob_values) + self.emission_cost[pos_type][word])
                 else:
-                    veterbi[index][pos_type] = (path,min(prob_values) + math.log(1/0.00001))
-            #print veterbi[index]
+                    veterbi[index][pos_type] = (path, min(prob_values) + math.log(1 / 0.00001))
+                    # print veterbi[index]
         pos_sent = []
         pro_sent = []
-        
-        for index in range(0,len(sentence)):
-           max_val = min(veterbi[index].iteritems(), key=operator.itemgetter(1))[0]
-           pos_sent.append(max_val)
-        min_index =  min(veterbi[len(sentence)-1])
-        final_pos =  veterbi[len(sentence)-1][min_index][0]
-        final_pos.append(min_index)
-        #print final_pos
-        return [[final_pos], []]
 
+        for index in range(0, len(sentence)):
+            max_val = min(veterbi[index].iteritems(), key=operator.itemgetter(1))[0]
+            pos_sent.append(max_val)
+        min_index = min(veterbi[len(sentence) - 1])
+        final_pos = veterbi[len(sentence) - 1][min_index][0]
+        final_pos.append(min_index)
+        # print final_pos
+        return [[final_pos], []]
 
     # This will be used for the variable elimination part and will be called in complex()
     def trans_prob(self, state, ind):
         if ind - 1 > 0:
             tau = trans_prob(self, state, ind - 1)
-        tau1 = tau*self.pos_transition_probabilities
+        tau1 = tau * self.pos_transition_probabilities
         return tau
 
     def complex(self, sentence):
